3768f35f01d0d4981f06318a1bc9af5f5ea8a404
Juwon Jin <makejin0222@gmail.com>
2017-04-12 09:14:42 +0900

    user action data collecting function

--- app/src/main/java/com/makejin/beautyproject_and/Splash/SplashActivity.java
+++ app/src/main/java/com/makejin/beautyproject_and/Splash/SplashActivity.java
@@ -6,10 +6,16 @@ import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.crashlytics.android.Crashlytics;
+import com.flurry.android.FlurryAgent;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+import com.makejin.beautyproject_and.AnalyticsApplication;
 import com.makejin.beautyproject_and.DressingTable.DressingTableActivity;
 import com.makejin.beautyproject_and.DressingTable.DressingTableActivity_;
 import com.makejin.beautyproject_and.Login.LoginActivity_;
@@ -25,6 +31,7 @@ import com.makejin.beautyproject_and.Utils.SharedManager.SharedManager;
 import com.facebook.internal.Utility;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
@@ -36,6 +43,7 @@ import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import io.fabric.sdk.android.Fabric;
 import io.userhabit.service.Userhabit;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
@@ -44,12 +52,29 @@ import rx.schedulers.Schedulers;
 @EActivity(R.layout.activity_splash)
 public class SplashActivity extends AppCompatActivity {
     SplashActivity activity;
+    AnalyticsApplication application = (AnalyticsApplication) getApplication();
+    Tracker mTracker = application.getDefaultTracker();
 
     @AfterViews
     void afterBindingView() {
         this.activity = this;
         Userhabit.start(this);
+
+
         Userhabit.setSessionEndTime(10);
+
+        new FlurryAgent.Builder()
+                .withLogEnabled(true)
+                .build(this, "HCPCS75FNYY8H97NSW7Q");
+
+        Fabric.with(this, new Crashlytics());
+
+        // TODO: Move this to where you establish a user session
+        logUser();
+
+
+
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -66,6 +91,21 @@ public class SplashActivity extends AppCompatActivity {
 
 
     }
+
+
+    private void logUser() {
+        // TODO: Use the current user's information
+        // You can call any combination of these three methods
+        Crashlytics.setUserIdentifier("12345");
+        Crashlytics.setUserEmail("user@fabric.io");
+        Crashlytics.setUserName("Test User");
+    }
+
+
+    public void forceCrash(View view) {
+        throw new RuntimeException("This is a crash");
+    }
+
 
     private final TimerTask spashScreenFinished = new TimerTask() {
         @Override
@@ -123,4 +163,18 @@ public class SplashActivity extends AppCompatActivity {
                 });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String name = "google analytics";
+        Log.i(name, "Setting screen name: " + name);
+        mTracker.setScreenName("Image~" + name);
+        mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+
+        mTracker.send(new HitBuilders.EventBuilder()
+                .setCategory("Action")
+                .setAction("Share")
+                .build());
+    }
 }
