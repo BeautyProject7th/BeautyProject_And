6fc0ae6c0ad9af479a9817999599c9dbcbc94360
lmjing <lmjing@naver.com>
2017-05-11 18:50:24 +0900

    피부타입 선택 디자인 적용

--- app/src/main/java/com/makejin/beautyproject_android/Skin/SkinTroubleActivity.java
+++ app/src/main/java/com/makejin/beautyproject_android/Skin/SkinTroubleActivity.java
@@ -1,11 +1,13 @@
 package com.makejin.beautyproject_android.Skin;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -37,6 +39,7 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 import static com.makejin.beautyproject_android.R.id.BT_next;
+import static com.makejin.beautyproject_android.R.id.LL_skin_trouble_9;
 
 
 @EActivity(R.layout.activity_skin_trouble)
@@ -51,6 +54,8 @@ public class SkinTroubleActivity extends ParentActivity {
     TextView TV_skin_trouble_1,TV_skin_trouble_2,TV_skin_trouble_3,TV_skin_trouble_4,TV_skin_trouble_5,TV_skin_trouble_6,TV_skin_trouble_7,TV_skin_trouble_8,TV_skin_trouble_9,toolbar_title;
 
     @ViewById
+    ImageView IV_skin_trouble_1,IV_skin_trouble_2,IV_skin_trouble_3,IV_skin_trouble_4,IV_skin_trouble_5,IV_skin_trouble_6,IV_skin_trouble_7,IV_skin_trouble_8,IV_skin_trouble_9;
+    @ViewById
     Button BT_complete, BT_back,BT_next;
 
     Integer skin_trouble = null;
@@ -58,6 +63,7 @@ public class SkinTroubleActivity extends ParentActivity {
     //LinearLayout id 저장하는 거임
     List<Integer> user_skin_trouble_list = new ArrayList<Integer>();
 
+    Map<Integer,int[]> skin_trouble_image = new HashMap<Integer, int[]>();
     Map<Integer,Trouble> skin_trouble_list = new HashMap<Integer, Trouble>();
 
     LinearLayout indicator;
@@ -93,6 +99,12 @@ public class SkinTroubleActivity extends ParentActivity {
         connectTestCall_update_skin_trouble(user, result);
     }
 
+    @Click({R.id.LL_skin_trouble_1,R.id.LL_skin_trouble_2,R.id.LL_skin_trouble_3,R.id.LL_skin_trouble_4,R.id.LL_skin_trouble_5, R.id.LL_skin_trouble_6,
+            R.id.LL_skin_trouble_7,R.id.LL_skin_trouble_8, LL_skin_trouble_9})
+    void onClick(View v){
+        click_skin_trouble(v.getId());
+    }
+
     @AfterViews
     void afterBindingView() {
         this.activity = this;
@@ -104,49 +116,75 @@ public class SkinTroubleActivity extends ParentActivity {
         Intent intent = getIntent();
         before_flag = intent.getBooleanExtra("before_login",false);
 
-        skin_trouble_list.put(R.id.LL_skin_trouble_1, new Trouble(TV_skin_trouble_1,"다크서클"));
-        skin_trouble_list.put(R.id.LL_skin_trouble_2, new Trouble(TV_skin_trouble_2,"블랙헤드"));
-        skin_trouble_list.put(R.id.LL_skin_trouble_3, new Trouble(TV_skin_trouble_3,"모공"));
-        skin_trouble_list.put(R.id.LL_skin_trouble_4, new Trouble(TV_skin_trouble_4,"각질"));
-        skin_trouble_list.put(R.id.LL_skin_trouble_5, new Trouble(TV_skin_trouble_5,"민감성"));
-        skin_trouble_list.put(R.id.LL_skin_trouble_6, new Trouble(TV_skin_trouble_6,"주름"));
-        skin_trouble_list.put(R.id.LL_skin_trouble_7, new Trouble(TV_skin_trouble_7,"여드름/트러블"));
-        skin_trouble_list.put(R.id.LL_skin_trouble_8, new Trouble(TV_skin_trouble_8,"안면홍조"));
-        skin_trouble_list.put(R.id.LL_skin_trouble_9, new Trouble(TV_skin_trouble_9,"없음"));
+        init_skintrouble_list();
+        init_trouble_image();
     }
 
-    @Click({R.id.LL_skin_trouble_1,R.id.LL_skin_trouble_2,R.id.LL_skin_trouble_3,R.id.LL_skin_trouble_4,R.id.LL_skin_trouble_5, R.id.LL_skin_trouble_6,
-            R.id.LL_skin_trouble_7,R.id.LL_skin_trouble_8,R.id.LL_skin_trouble_9})
-    void onClick(View v){
-        click_skin_trouble(v.getId());
+    private void init_trouble_image() {
+        skin_trouble_image.put(R.id.LL_skin_trouble_1, getImageList(R.drawable.trouble1_darkcircle,R.drawable.trouble1_select));
+        skin_trouble_image.put(R.id.LL_skin_trouble_2, getImageList(R.drawable.trouble2_blackhead,R.drawable.trouble2_select));
+        skin_trouble_image.put(R.id.LL_skin_trouble_3, getImageList(R.drawable.trouble3_pore,R.drawable.trouble3_select));
+        skin_trouble_image.put(R.id.LL_skin_trouble_4, getImageList(R.drawable.trouble4_deadskin,R.drawable.trouble4_select));
+        skin_trouble_image.put(R.id.LL_skin_trouble_5, getImageList(R.drawable.trouble5_sensitivity,R.drawable.trouble5_select));
+        skin_trouble_image.put(R.id.LL_skin_trouble_6, getImageList(R.drawable.trouble6_wrinkle,R.drawable.trouble6_select));
+        skin_trouble_image.put(R.id.LL_skin_trouble_7, getImageList(R.drawable.trouble7_acne,R.drawable.trouble7_select));
+        skin_trouble_image.put(R.id.LL_skin_trouble_8, getImageList(R.drawable.trouble8_flush,R.drawable.trouble8_selec));
+        skin_trouble_image.put(LL_skin_trouble_9, getImageList(R.drawable.trouble9_nothing,R.drawable.trouble9_select));
     }
 
+    private int[] getImageList(int one,int two){
+        int[] skintrouble = new int[2];
+        skintrouble[0] = one;
+        skintrouble[1] = two;
+        return skintrouble;
+    }
+
+    private void init_skintrouble_list() {
+        skin_trouble_list.put(R.id.LL_skin_trouble_1, new Trouble(TV_skin_trouble_1,"다크서클",IV_skin_trouble_1));
+        skin_trouble_list.put(R.id.LL_skin_trouble_2, new Trouble(TV_skin_trouble_2,"블랙헤드",IV_skin_trouble_2));
+        skin_trouble_list.put(R.id.LL_skin_trouble_3, new Trouble(TV_skin_trouble_3,"모공",IV_skin_trouble_3));
+        skin_trouble_list.put(R.id.LL_skin_trouble_4, new Trouble(TV_skin_trouble_4,"각질",IV_skin_trouble_4));
+        skin_trouble_list.put(R.id.LL_skin_trouble_5, new Trouble(TV_skin_trouble_5,"민감성",IV_skin_trouble_5));
+        skin_trouble_list.put(R.id.LL_skin_trouble_6, new Trouble(TV_skin_trouble_6,"주름",IV_skin_trouble_6));
+        skin_trouble_list.put(R.id.LL_skin_trouble_7, new Trouble(TV_skin_trouble_7,"여드름/트러블",IV_skin_trouble_7));
+        skin_trouble_list.put(R.id.LL_skin_trouble_8, new Trouble(TV_skin_trouble_8,"안면홍조",IV_skin_trouble_8));
+        skin_trouble_list.put(LL_skin_trouble_9, new Trouble(TV_skin_trouble_9,"없음",IV_skin_trouble_9));
+    }
 
     void click_skin_trouble(int view_id){
         skin_trouble = view_id;
 
         if(user_skin_trouble_list.contains(skin_trouble)) {//피부고민 취소
             user_skin_trouble_list.remove(skin_trouble);
-            skin_trouble_list.get(view_id).TV_trouble.setTextColor(getResources().getColor(R.color.colorBlackText));
-        }else if(skin_trouble == R.id.LL_skin_trouble_9){ //없음 선택시 다른 버튼 모두 취소됨(단일 선택이기 때문)
+            unselect_trouble(skin_trouble);
+        }else if(skin_trouble == LL_skin_trouble_9){ //없음 선택시 다른 버튼 모두 취소됨(단일 선택이기 때문)
             for (Integer trouble:user_skin_trouble_list){
-                skin_trouble_list.get(trouble).TV_trouble.setTextColor(getResources().getColor(R.color.colorBlackText));
+                unselect_trouble(trouble);
             }
             user_skin_trouble_list.removeAll(user_skin_trouble_list);
 
-            skin_trouble_list.get(view_id).TV_trouble.setTextColor(getResources().getColor(R.color.colorAccent));
-            user_skin_trouble_list.add(view_id);
+            select_trouble(skin_trouble);
         }else if(user_skin_trouble_list.size()>=3){//피부고민 3개 이상일때
             Snackbar.make(TV_skin_trouble_9,"피부고민 사항은 최대 3개까지 선택 가능합니다.",Snackbar.LENGTH_SHORT).show();
         }else{//아니면 고민항목에 담기
             //없음 선택된 경우 없음 체크 해제
-            if(user_skin_trouble_list.contains(R.id.LL_skin_trouble_9)){
-                TV_skin_trouble_9.setTextColor(getResources().getColor(R.color.colorBlackText));
+            if(user_skin_trouble_list.contains(LL_skin_trouble_9)){
+                unselect_trouble(LL_skin_trouble_9);
                 user_skin_trouble_list.removeAll(user_skin_trouble_list);
             }
-            user_skin_trouble_list.add(skin_trouble);
-            skin_trouble_list.get(view_id).TV_trouble.setTextColor(getResources().getColor(R.color.colorAccent));
+            select_trouble(skin_trouble);
         }
+    }
+
+    private void unselect_trouble(int view_id) {
+        skin_trouble_list.get(view_id).TV_trouble.setTextColor(getResources().getColor(R.color.colorBlackText));
+        skin_trouble_list.get(view_id).IV_trouble.setImageDrawable(getResources().getDrawable(skin_trouble_image.get(view_id)[0]));
+    }
+
+    private void select_trouble(int view_id) {
+        skin_trouble_list.get(view_id).TV_trouble.setTextColor(getResources().getColor(R.color.colorAccent));
+        user_skin_trouble_list.add(view_id);
+        skin_trouble_list.get(view_id).IV_trouble.setImageDrawable(getResources().getDrawable(skin_trouble_image.get(view_id)[1]));
     }
 
     @UiThread
@@ -196,9 +234,11 @@ public class SkinTroubleActivity extends ParentActivity {
 class Trouble{
     public TextView TV_trouble;
     public String trouble;
+    public ImageView IV_trouble;
 
-    Trouble(TextView textView, String trouble){
+    Trouble(TextView textView, String trouble, ImageView imageView){
         this.TV_trouble = textView;
         this.trouble = trouble;
+        this.IV_trouble = imageView;
     }
 }
